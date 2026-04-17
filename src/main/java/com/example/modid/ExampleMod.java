b3e6e8ef69910f205942595a74d73a1835777902
Bron1t <123360173+Bronitt@users.noreply.github.com>
2026-04-17 14:14:23 +0300

    Initial commit

---
+++ src/main/java/com/example/modid/ExampleMod.java
@@ -0,0 +1,43 @@
+package com.example.modid;
+
+import com.example.modid.proxy.CommonProxy;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = Tags.MOD_ID, name = Tags.MOD_NAME, version = Tags.VERSION, dependencies = Tags.DEPENDENCIES)
+public class ExampleMod {
+
+    public static final Logger LOGGER = LogManager.getLogger(Tags.MOD_NAME);
+
+    @SidedProxy(
+            modId = Tags.MOD_ID,
+            clientSide = Tags.CLIENT_PROXY_PATH,
+            serverSide = Tags.SERVER_PROXY_PATH
+    )
+    public static CommonProxy proxy;
+
+    @Instance
+    public static ExampleMod instance;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        proxy.preInit(event);
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+        proxy.init(event);
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+        proxy.postInit(event);
+    }
+}
